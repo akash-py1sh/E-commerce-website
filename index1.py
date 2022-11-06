95fa641307159e8b2054b61b3241623cf6e7e00a
Akash <mymother472@gamil.com>
2022-11-06 17:10:29 +0530

    gfgf

--- index1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-from ipywidgets import interact
-df = pd.read_csv("data.csv")
-
-print(df.isnull().sum())
