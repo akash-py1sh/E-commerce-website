a9be58123c928e497da5c82dda3c9aa77e874fda
Akash <mymother472@gamil.com>
2022-11-09 19:48:15 +0530

    gfgf

--- backend/model/category_mod.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const catschema=mongoose.Schema({
-    cat_name:String
-});
-
-module.exports=mongoose.model('Category',catschema);
--- backend/model/product_mod.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const pschema=mongoose.Schema({
-    category:mongoose.Schema.Types.ObjectId,
-    pname:String,
-    pprice:Number,
-    pimg:String
-});
-
-module.exports=mongoose.model('Product',pschema);
